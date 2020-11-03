8b71310e9164a15a66d1bd5929eb7e9b59ed626e
saltyfoxy <nerdgbx@gmail.com>
2020-11-03 12:22:57 +0100

    ajout bouton connexion et inscription avec redirection vers pages respectives, formulaire html+css non fonctionnel car je sais plus faire oups, je suis paumé sans Symfony, help

---
+++ BDDWORKSHOP.sql
@@ -0,0 +1,166 @@
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
+--
+-- Host: 192.168.1.26    Database: greencity
+-- ------------------------------------------------------
+-- Server version	5.7.32-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `QestScenario`
+--
+
+DROP TABLE IF EXISTS `QestScenario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `QestScenario` (
+  `idScenario` int(11) DEFAULT NULL,
+  `idQuestion` int(11) DEFAULT NULL,
+  KEY `QestScenario_FK` (`idScenario`),
+  KEY `QestScenario_FK_1` (`idQuestion`),
+  CONSTRAINT `QestScenario_FK` FOREIGN KEY (`idScenario`) REFERENCES `scenario` (`id`),
+  CONSTRAINT `QestScenario_FK_1` FOREIGN KEY (`idQuestion`) REFERENCES `question` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `QestScenario`
+--
+
+LOCK TABLES `QestScenario` WRITE;
+/*!40000 ALTER TABLE `QestScenario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `QestScenario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `question`
+--
+
+DROP TABLE IF EXISTS `question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `question` (
+  `id` int(11) NOT NULL,
+  `nText` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `question`
+--
+
+LOCK TABLES `question` WRITE;
+/*!40000 ALTER TABLE `question` DISABLE KEYS */;
+/*!40000 ALTER TABLE `question` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reponse`
+--
+
+DROP TABLE IF EXISTS `reponse`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reponse` (
+  `id` int(11) NOT NULL,
+  `nText` varchar(100) NOT NULL,
+  `points` int(11) NOT NULL,
+  `idQuestion` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `reponse_FK` (`idQuestion`),
+  CONSTRAINT `reponse_FK` FOREIGN KEY (`idQuestion`) REFERENCES `question` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reponse`
+--
+
+LOCK TABLES `reponse` WRITE;
+/*!40000 ALTER TABLE `reponse` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reponse` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `scenario`
+--
+
+DROP TABLE IF EXISTS `scenario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `scenario` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) DEFAULT NULL,
+  `dificulte` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `scenario`
+--
+
+LOCK TABLES `scenario` WRITE;
+/*!40000 ALTER TABLE `scenario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `scenario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `utilisateur` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) NOT NULL,
+  `prenom` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `date_naissance` date NOT NULL,
+  `pays` varchar(255) DEFAULT NULL,
+  `ville` varchar(255) DEFAULT NULL,
+  `code_postal` varchar(5) DEFAULT NULL,
+  `rang` varchar(100) DEFAULT NULL,
+  `password` varchar(100) NOT NULL,
+  `score` int(11) DEFAULT NULL,
+  `idScenario` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `utilisateur_FK` (`idScenario`),
+  CONSTRAINT `utilisateur_FK` FOREIGN KEY (`idScenario`) REFERENCES `scenario` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `utilisateur`
+--
+
+LOCK TABLES `utilisateur` WRITE;
+/*!40000 ALTER TABLE `utilisateur` DISABLE KEYS */;
+/*!40000 ALTER TABLE `utilisateur` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'greencity'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-03 10:26:36
