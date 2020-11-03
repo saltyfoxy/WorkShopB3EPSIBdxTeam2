e90a14ae7a55cb0611b1bfcad146d257c273a520
Blaayo <ayoub.blanchet@epsi.fr>
2020-11-03 11:20:26 +0100

    Ajout scripte BDD
    scripte pour se connecter à la bdd

---
+++ include/bdd.php
@@ -0,0 +1,15 @@
+<?php
+    $dbConn = null;
+    try{
+        /*
+         * Ouverture de la connexion à la base de données
+         */
+        $dbConn = new PDO('mysql:host=localhost;port=3306;charset=utf8;dbname=greencity', 'root', 'root');
+    }catch (PDOException $ex){
+        /*
+         * En cas d'erreur, gestion d'une exception (à voir plus tard)
+         */
+        header('Location: index.html');
+        exit();
+    }
+?>
